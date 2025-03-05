c853e1c196c35e9017123a1659f750475307dddc
akshat799 <gulati.akshat799@gmail.com>
2025-03-05 15:22:41 -0400

    Red(Setting Task Class) ]

---
+++ test/TaskTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+public class TaskTest {
+
+	private Task task;
+	@Before
+	public void setUp(){
+		task = new Task();
+	}
+}
