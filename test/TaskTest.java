6cdbc3a9a532fd6db2a2732f64c8fb779f74939f
akshat799 <gulati.akshat799@gmail.com>
2025-03-05 15:25:35 -0400

    Red(Creating assignTo method)

--- test/TaskTest.java
+++ test/TaskTest.java
@@ -1,11 +1,21 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TaskTest {
 
 	private Task task;
 	@Before
 	public void setUp(){
 		task = new Task();
+	}
+
+	@Test
+	public void testAssignTo(){
+		String name = "Akshat Gulati";
+		task.assignTo(name);
+		assertEquals(name,task.getTeamMember);
 	}
 }
