ddd931e301a2511f589951b3bd081ee8d586495e
akshat799 <gulati.akshat799@gmail.com>
2025-03-05 15:32:57 -0400

    Green(updateStatus is working)

--- src/Task.java
+++ src/Task.java
@@ -1,11 +1,20 @@
 public class Task {
 	private String teamMember;
+	private String status;
 
 	public void assignTo(String name){
 		this.teamMember = name;
 	}
 
+	public void updateStatus(String status){
+		this.status = status;
+	}
+
 	public String getTeamMember() {
 		return teamMember;
+	}
+
+	public String getStatus() {
+		return status;
 	}
 }
